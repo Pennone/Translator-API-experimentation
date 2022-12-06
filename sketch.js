005440b5ed8574a70f6e4cf763080133dd683a5b
Pennone <fabio.pennone@mail.polimi.it>
2022-12-07 00:38:15 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,6 +9,43 @@ function setup() {
 
 }
 
+const translate_api_endpoint = "https://api.cognitive.microsofttranslator.com";
+let translate_api_key = prompt("Enter your API key:");
+const translate_version      = "3.0";
+const translate_region       = "switzerlandnorth";
+
+let language = "en";
+let sentence = "";
+let language_to = "fr";
+
+async function translate(source_text, target_language) {
+  const endpoint = `${translate_api_endpoint}/translate?api-version=${translate_version}&to=${target_language}`; // Constructing the URL to send to
+  const data_body = [{'text': source_text}]; // Constructing the data to be sent
+  const response = await fetch(endpoint, {
+    method: 'POST', // We will use POST to send this data to the end point
+    mode: 'cors', // CORS is a security feature that only allows requests to/from the same site, in this case because we're sending data to an external site we will turn it off
+    cache: 'no-cache', // We will disable caching so that we will always get the "fresh" response from the server
+    headers: {
+      'Content-Type': 'application/json',
+      'Ocp-Apim-Subscription-Key': translate_api_key,
+      'Ocp-Apim-Subscription-Region': translate_region
+    },
+    body: JSON.stringify(data_body)
+  });
+  return response.json(); // parse json response into a javascript object and return
+}
+
 function draw() {
 
+}
+
+function mouseClicked() {
+  //language = prompt("Enter starting language:");
+  sentence = prompt("Enter sentence to translate:");
+  language_to = prompt("Enter language to translate to:");
+
+  translate(sentence, language_to).then((data) => {
+    const result = data[0]['translations'][0]['text'];
+    console.log(result);
+  });
 }
